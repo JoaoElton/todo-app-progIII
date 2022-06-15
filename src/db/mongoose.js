3d97af99d6de326f2cd5b84abca2289fa9396c2d
Elton Santana <92764635+JoaoElton@users.noreply.github.com>
2022-06-15 00:09:08 -0300

    feat: cadastro usuario

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/test';
-
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
+mongoose.connect("mongodb://localhost:27017/task-manager-api", {
+  useNewUrlParser: true
 });
--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
-const bcrypt = require('bcrypt');
 
 require('./db/mongoose');
 
 const app = require('./app');
 
-const port = process.env['PORT'] || 8000;
+const port = 3333;
 
 app.listen(port, () => {
   console.log(`Server running and listen on port ${port}...`);
